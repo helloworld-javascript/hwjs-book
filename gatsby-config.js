7a356a39eac59732bf88a9a2d9742a4e5cd6b674
Sehyun Chung <sehyun.chung@icloud.com>
2021-02-28 16:28:53 +0900

    feat: add path resolver

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,6 @@
 const {generateConfig} = require('gatsby-plugin-ts-config')
+
 module.exports = generateConfig({
-  configDir: 'gatsbyConfigs', // or wherever you would like to store your gatsby files
+  configDir: 'gatsby', // or wherever you would like to store your gatsby files
   projectRoot: __dirname,
 })
