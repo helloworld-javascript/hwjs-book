f38a669c7a67d4f3a401b4fb2a766706144caa6e
Sehyun Chung <sehyun.chung@icloud.com>
2021-02-28 15:57:10 +0900

    feat: enable typescript & change npm -> yarn

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,5 @@
-module.exports = {
-  siteMetadata: {
-    title: 'JavaScript로 만나는 세상',
-    shortName: 'JavaScript로 만나는 세상',
-    description:
-      '본 교재는 [패스트캠퍼스 프론트엔드 개발 스쿨](http://school.fastcampus.co.kr/dev_fds/)의 강의 자료로서 제작되었습니다. 강의에서는 이 교재에 나온 내용 외에 실무 관련 지식을 포함한 더 깊은 내용을 다루며, 퀴즈, 실습 과제 수행, 코드 리뷰, 팀 프로젝트 등의 활동을 하게 됩니다.',
-  },
-  plugins: ['@primer/gatsby-theme-doctocat'],
-}
+const {generateConfig} = require('gatsby-plugin-ts-config')
+module.exports = generateConfig({
+  configDir: 'gatsbyConfigs', // or wherever you would like to store your gatsby files
+  projectRoot: __dirname,
+})
